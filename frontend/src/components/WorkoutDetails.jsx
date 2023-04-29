adb289b2cd2a099e34999680574f19b346a64aa4
Lil-Adonis <adenugaib24@gmail.com>
2023-04-29 11:53:43 -1100

    :ambulance: dispatch fix , date fns
    date fns

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+// date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const WorkoutDetails = ({ workout, key }) => {
+    const { dispatch } = useWorkoutsContext()
     const handleClick = async () => {
         const response = await fetch('/api/workouts/' + workout._id, {
           method: 'DELETE'
@@ -17,7 +20,7 @@ const WorkoutDetails = ({ workout, key }) => {
       <h4>{workout.title}</h4>
       <p><strong>Load (kg): </strong>{workout.load}</p>
       <p><strong>Number of reps: </strong>{workout.reps}</p>
-      <p>{workout.createdAt}</p>
+      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
       <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
     </div>
   )
